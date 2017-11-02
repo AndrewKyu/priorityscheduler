6f3180a5c3bd2b78ee16d8103db593f1e579bb04
Cindy Wang <cindykathleenwang@gmail.com>
2017-11-02 11:40:37 -0700

    Menu
    Basic menu layout set up

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -11,5 +11,51 @@ using namespace std;
 
 int main()
 {
-    // Menu
+    int x;
+    
+    while (true)
+    {
+        // Menu
+        cout << "-------------------------------------------" << endl;
+        cout << "MENU: " << endl;
+        cout << "-------------------------------------------" << endl;
+        cout << "1. Add a Task" << endl;
+        cout << "2. Prioritize by using First Come First Serve " << endl;
+        cout << "3. Prioritize by using Shortest Job First " << endl;
+        cout << "4. Prioritize by using Priority Weight " << endl;
+        cout << "5. Prioritize by using Round Robin " << endl;
+        cout << "6. Prioritize by using Multilevel Queue Scheduling " << endl;
+        cout << "7. Quit Program" << endl;
+        cout << "-------------------------------------------" << endl;
+        cin >> x;
+        
+        if (x == 1)
+        {
+        }
+        else if (x == 2)
+        {
+        }
+        else if (x == 3)
+        {
+        }
+        else if (x == 4)
+        {
+        }
+        else if (x == 5)
+        {
+        }
+        else if (x == 6)
+        {
+        }
+        else if (x == 7)
+        {
+            cout << "Program Ending" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Please insert a valid option." << endl;
+            continue;
+        }
+    }
 }
