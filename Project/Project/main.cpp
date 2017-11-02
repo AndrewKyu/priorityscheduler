64136dcf1d9ccda148ccccbbe3c57573ae921de8
Cindy Wang <cindykathleenwang@gmail.com>
2017-11-02 11:34:40 -0700

    Initial Files Created
    Xcode

---
+++ Project/Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Cindy Wang on 11/2/17.
+//  Copyright © 2017 Cindy Wang. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Menu
+}
