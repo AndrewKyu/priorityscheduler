96a4e71f1487c9c43cc650e257412262104742c0
Cindy Wang <cindykathleenwang@gmail.com>
2017-11-15 18:31:44 -0800

    Menu

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -13,45 +13,48 @@ int main()
 {
     int x;
     
+    // user chooses how they would like their tasks to be sorted
     while (true)
     {
         // Menu
         cout << "-------------------------------------------" << endl;
-        cout << "MENU: " << endl;
+        cout << "HOW WOULD YOU LIKE YOUR TASKS SORTED: " << endl;
         cout << "-------------------------------------------" << endl;
-        cout << "1. Add a Task" << endl;
-        cout << "2. Prioritize by using First Come First Serve " << endl;
-        cout << "3. Prioritize by using Shortest Job First " << endl;
-        cout << "4. Prioritize by using Priority Weight " << endl;
-        cout << "5. Prioritize by using Round Robin " << endl;
-        cout << "6. Prioritize by using Multilevel Queue Scheduling " << endl;
-        cout << "7. Quit Program" << endl;
+        cout << "1. Prioritize by using First Come First Serve " << endl;
+        cout << "2. Prioritize by using Shortest Job First " << endl;
+        cout << "3. Prioritize by using Priority Weight " << endl;
+        cout << "4. Prioritize by using Round Robin " << endl;
+        cout << "5. Prioritize by using Multilevel Queue Scheduling " << endl;
+        cout << "6. Quit Program" << endl;
         cout << "-------------------------------------------" << endl;
         cin >> x;
         
         if (x == 1)
         {
         }
+        
         else if (x == 2)
         {
         }
+        
         else if (x == 3)
         {
         }
+        
         else if (x == 4)
         {
         }
+        
         else if (x == 5)
         {
         }
+        
         else if (x == 6)
-        {
-        }
-        else if (x == 7)
         {
             cout << "Program Ending" << endl;
             break;
         }
+        
         else
         {
             cout << "Please insert a valid option." << endl;
