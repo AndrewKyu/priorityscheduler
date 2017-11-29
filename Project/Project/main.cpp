270f4d371c83934c343114266cb083aa35df5214
Cindy Wang <cindykathleenwang@gmail.com>
2017-11-28 20:20:14 -0800

    Multilevel Queue
    Multilevel Queue Implementation

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -17,16 +17,16 @@ int main()
     while (true)
     {
         // Menu
-        cout << "-------------------------------------------" << endl;
+        cout << "---------------------------------------------------" << endl;
         cout << "HOW WOULD YOU LIKE YOUR TASKS SORTED: " << endl;
-        cout << "-------------------------------------------" << endl;
+        cout << "---------------------------------------------------" << endl;
         cout << "1. Prioritize by using First Come First Serve " << endl;
         cout << "2. Prioritize by using Shortest Job First " << endl;
         cout << "3. Prioritize by using Priority Weight " << endl;
         cout << "4. Prioritize by using Round Robin " << endl;
         cout << "5. Prioritize by using Multilevel Queue Scheduling " << endl;
         cout << "6. Quit Program" << endl;
-        cout << "-------------------------------------------" << endl;
+        cout << "---------------------------------------------------" << endl;
         cin >> x;
         
         if (x == 1)
---
+++ Project/Project/multilevelQueue.hpp
@@ -0,0 +1,16 @@
+//
+//  multilevelQueue.hpp
+//  Project
+//
+//  Created by Cindy Wang on 11/28/17.
+//  Copyright © 2017 Cindy Wang. All rights reserved.
+//
+
+#ifndef multilevelQueue_hpp
+#define multilevelQueue_hpp
+
+#include <stdio.h>
+
+void doMultilevelQueue()
+
+#endif /* multilevelQueue_hpp */
